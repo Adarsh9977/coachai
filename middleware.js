21e94256fb13be8e17a9076cac9b5301671f0379
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-02-22 09:45:59 +0530

    fixed middleware logic

--- middleware.js
+++ middleware.js
@@ -10,10 +10,9 @@ const isProtectedRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth, req)=>{
-    const { userId } = await auth()
+    const { userId, redirectToSignIn } = await auth()
 
     if(!userId && isProtectedRoute(req)){
-        const {redirectToSignIn} = await auth();
         return redirectToSignIn();
     }
 
@@ -21,10 +20,5 @@ export default clerkMiddleware(async (auth, req)=>{
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
