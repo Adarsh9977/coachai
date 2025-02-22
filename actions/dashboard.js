2e3b1d3d8f29b426e3f2c5bbd0b466275e37c8fb
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2025-02-22 09:26:41 +0530

    update the prompt

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -27,7 +27,7 @@ export const generateAIInsights =  async (industry) => {
           IMPORTANT: Return ONLY the JSON. No additional text, notes, or markdown formatting.
           Include at least 5 common roles for salary ranges.
           Growth rate should be a percentage.
-          Include at least 5 skills and trends.
+          Include at least 5 skills and trends and more you can add till 15.
         `;
 
         const result = await model.generateContent(prompt);
